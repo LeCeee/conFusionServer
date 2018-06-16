ca9d8e6dbfe4a8a08dcda884f263b99819c39a45
queenCee <aakarshitatiwari@gmail.com>
2018-06-16 16:59:59 +0530

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
-
+const Dishes = require('./model/dishes');
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+connect.then((db) => {
+  console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 var app = express();
 
 // view engine setup
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,43 +1,84 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Dishes = require('../model/dishes');
+
 const dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-})
 .get((req,res,next) => {
-    res.end('will send all the dishes to you!');
+    Dishes.find({})
+    .then((dishes) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dishes);
+    }, (err) => next(err))
+    .catch((err) =>next(err));
 })
 .post((req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+    Dishes.create(req.body)
+    .then((dish) => {
+        console.log('Dish Created',dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) =>  next(err));
+    //res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
 })
 .put((req,res,next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported');
 })
 .delete((req,res,next) => {
-    res.end('will delete all the dishes for you');
+    Dishes.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+    //res.end('will delete all the dishes for you');
 });
 
 
 dishRouter.route('/:dishId')
 .get((req,res,next) => {
-    res.end('will get the details of the dish : ' + req.params.dishId + ' to you');
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+    },(err) => next(err))
+    .catch((err) => next(err));
+    //res.end('will get the details of the dish : ' + req.params.dishId + ' to you');
 })
 .post((req,res,next) => {
     res.statusCode = 403;
     res.end('post operation not supported on /dishes/'+ req.params.dishId);
 })
 .put((req,res,next) => {
-    res.write('updating the dish: '+ req.params.dishId + '\n');
-    res.end('will update the dish: ' + req.body.name + ' with details ' + req.body.description);
+    Dishes.findByIdAndUpdate(req.params.dishId,{
+        $set:req.body
+    },{ new: true })
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+    },(err) => next(err))
+    .catch((err) => next(err));
 })
 .delete((req,res,next) => {
-    res.end('will delete the dish : ' + req.params.dishId + ' for you');
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(resp);
+    },(err) => next(err))
+    .catch((err) => next(err));
+    //res.end('will delete the dish : ' + req.params.dishId + ' for you');
 });
 
 
