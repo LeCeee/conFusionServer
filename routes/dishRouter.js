ad62922b259c4703b79ddfbda49daf5519cbebe3
queenCee <aakarshitatiwari@gmail.com>
2018-06-24 17:53:05 +0530

    mongoose population

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -9,7 +9,8 @@ dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
 .get((req,res,next) => {
-    Dishes.find({})
+	Dishes.find({})
+	.populate('comments.author')
     .then((dishes) => {
         res.statusCode = 200;
         res.setHeader('Content-Type','application/json');
@@ -46,8 +47,10 @@ dishRouter.route('/')
 
 dishRouter.route('/:dishId')
 .get((req,res,next) => {
+
     Dishes.findById(req.params.dishId)
-    .then((dish) => {
+	.populate('comments.author')
+	.then((dish) => {
         res.statusCode = 200;
         res.setHeader('Content-Type','application/json');
         res.json(dish);
@@ -84,6 +87,7 @@ dishRouter.route('/:dishId')
 dishRouter.route('/:dishId/comments')
 .get((req,res,next) => {
 	Dishes.findById(req.params.dishId)
+	.populate('comments.author')
 		.then((dish) => {
 			if(dish !=  null){
 				res.statusCode = 200;
@@ -102,6 +106,7 @@ dishRouter.route('/:dishId/comments')
 	Dishes.findById(req.params.dishId)
 	.then((dish) => {
 		if(dish !=  null){
+			req.body.author = req.user._id;
 			dish.comments.push(req.body);
 			dish.save()
 			.then((dish) => {
@@ -149,6 +154,7 @@ dishRouter.route('/:dishId/comments')
 dishRouter.route('/:dishId/comments/:commentId')
 .get((req,res,next) => {
 	Dishes.findById(req.params.dishId)
+	.populate('comments.author')
 	.then((dish) => {
 		if(dish !=  null && dish.comments.id(req.params.commentId) != null){
 			res.statusCode = 200;
