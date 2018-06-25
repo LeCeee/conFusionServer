159c0553139db72f8933d5b23857d1d140ec3390
queenCee <aakarshitatiwari@gmail.com>
2018-06-25 14:41:01 +0530

    task 1 as.3

--- authenticate.js
+++ authenticate.js
@@ -50,3 +50,13 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
 //     })
 //   );
     exports.verifyUser = passport.authenticate('jwt',{session: false});
+    exports.verifyAdmin = (req,res,next) => {
+        if(req.user.admin)
+            next();
+            
+        else {
+            var err = new Error('you are not an admin');
+            err.status = 403;
+            return next(err);
+        }
+    }
